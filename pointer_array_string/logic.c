0ebe405d59e1d95af1c0e097b9c6aba9833d509b
renisv <11432@holbertonstudents.com>
2025-07-04 12:11:08 +0000

    update

---
+++ pointer_array_string/logic.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "main.h"
+
+int count_vowel(char *str)
+{
+    int i,j;
+    int count = 0;
+    char vowel[] = "aeiouyAEIOUY";
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        for (j = 0; vowel[j] != '\0'; j++)
+        {
+            if (str[i] == vowel[j])
+            {
+                count++;
+                break;
+            }
+        }
+    }
+    return count;
+}
+
+void swap_int(int *num1, int *num2)
+{
+    int temp = *num1;
+    *num1 = *num2;
+    *num2 = temp;
+}
+
+int find_min(int arr[], int size)
+{
+    if (size <= 0) {
+        return 0; 
+    }
+
+    int min = arr[0]; 
+    
+    for (int i = 1; i < size; i++) {
+        if (arr[i] < min) {
+            min = arr[i]; 
+        }
+    }
+    
+    return min;
+}
---
+++ pointer_array_string/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+int main(void)
+{
+    char str[] = {"String Test"};
+    int result = count_vowel(str);
+    printf("The number of vowels in the string %s is %d.\n", str, result);
+
+    int a = 5, b = 10;
+    printf("Before: a = %d, b = %d\n", a, b);
+    swap_int(&a, &b);
+    printf("After: a = %d, b = %d\n", a, b);
+
+    int numbers[] = {34, 12, 56, 2, 89};
+    int size = sizeof(numbers) / sizeof(numbers[0]);
+    
+    int minimum = find_min(numbers, size);
+    printf("The smallest number is: %d\n", minimum);
+    
+}
