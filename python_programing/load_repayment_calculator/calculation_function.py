1c0ff99d18c93d11f6180cfb0e9250bfe4239195
renisv <11432@holbertonstudents.com>
2025-09-08 06:55:19 +0000

    update

--- python_programing/load_repayment_calculator/calculation_function.py
+++ python_programing/load_repayment_calculator/calculation_function.py
@@ -3,7 +3,7 @@ from input_functions import get_annual_interest_rate, get_loan_amount, get_loan_
 def calculate_repayment():
     loan_amount = get_loan_amount()
     loan_term_in_months = get_loan_term() * 12
-    monthly_interest_rate = get_annual_interest_rate() / 100 / 12  # Note: Fixed spelling and added /100
+    monthly_interest_rate = get_annual_interest_rate() / 100 / 12 
 
     numerator = loan_amount * monthly_interest_rate * (1 + monthly_interest_rate) ** loan_term_in_months
     denominator = (1 + monthly_interest_rate) ** loan_term_in_months - 1
